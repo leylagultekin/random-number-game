e5c04d8713b45a1a4a5688b26357e4464d28321e
Leyla Gultekin <117444314+leylagultekin@users.noreply.github.com>
2022-11-04 12:34:26 +0000

    difference from answer function

--- main.py
+++ main.py
@@ -2,5 +2,11 @@ num = 0
 import random
 def generate_random_number():
   return random.randint(0,100)
-  
-print(generate_random_number())
+
+def difference_from_answer(guess, answer):
+  if guess == answer:
+    return "Correct"
+  elif guess > answer:
+    return "Too high"
+  elif guess < answer:
+    return "Too low"
