d9a5fe7c624c2d2708353e098ed4770d08a9e304
Leyla Gultekin <117444314+leylagultekin@users.noreply.github.com>
2022-11-04 12:52:01 +0000

    added function make a guess

--- main.py
+++ main.py
@@ -10,3 +10,7 @@ def difference_from_answer(guess, answer):
     return "Too high"
   elif guess < answer:
     return "Too low"
+
+def make_a_guess (answer):
+  guess = input("Guess a number between 0-100")
+  difference_from_answer(guess, answer)
